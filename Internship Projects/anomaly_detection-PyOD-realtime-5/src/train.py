c2feecf9f931da528dc171e9b78c0d403e01c951
Garvit Rastogi <111786577+garvitr02@users.noreply.github.com>
2024-07-04 16:47:33 +0530

    Update train.py
    If already trained model exists, then the model does not get trained again on start, it uses the same model.

--- Internship Projects/anomaly_detection-PyOD-realtime-5/src/train.py
+++ Internship Projects/anomaly_detection-PyOD-realtime-5/src/train.py
@@ -1,26 +1,28 @@
+import os
 import time
 import numpy as np
+from sklearn.ensemble import IsolationForest
 import joblib
-from pyod.models.iforest import IForest
 from utils import get_real_time_cpu_usage
 
 def train_model(duration=300):
-    print("Starting model training...")
-    end_time = time.time() + duration
-    data = []
-
-    while time.time() < end_time:
-        cpu_usage = get_real_time_cpu_usage()
-        data.append(cpu_usage)
-        time.sleep(1)  # Collect data every second
-
-    X = np.array(data).reshape(-1, 1)
-    model = IForest()
-    model.fit(X)
+    model_path = '../data/trained_model.pkl'
     
-    # Save the model
-    joblib.dump(model, '/usr/src/app/data/trained_model.pkl')
-    print("Model trained and saved.")
+    if os.path.exists(model_path):
+        print(f"Model already exists at {model_path}. Skipping training.")
+        return
 
-if __name__ == "__main__":
-    train_model()
+    print("Starting model training...")
+    start_time = time.time()
+    cpu_usages = []
+
+    while time.time() - start_time < duration:
+        cpu_usage = get_real_time_cpu_usage()
+        cpu_usages.append(cpu_usage)
+        time.sleep(1)
+
+    model = IsolationForest(contamination=0.1)
+    model.fit(np.array(cpu_usages).reshape(-1, 1))
+
+    joblib.dump(model, model_path)
+    print("Model trained and saved.")
